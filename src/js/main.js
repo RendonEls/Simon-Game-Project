2f577347f2fc7a5b36708fe5629d1360a0a1abf2
Rendon Elsesser <rendonelsesser@gmail.com>
2019-01-09 15:40:08 -0500

    timer works, opacity style change was not a good idea, working on solutions

--- src/js/main.js
+++ src/js/main.js
@@ -1,74 +1,70 @@
 
 
-const lights = document.querySelectorAll(".light")
+const divColorArray = document.querySelectorAll(".light")
 const startGame = document.getElementById("startButton")
 const restartGame = document.getElementById("restartButton")
 const playerSelection = []
-const colorArray = ["red", "green", "blue", "yellow"]
+const availableColors = ["red", "green", "blue", "yellow"]
 let turnCount = 0
-const randomColorArray = randomColor() // array of randomcolors
-
+const randomColorArray = makeRandomColor() // array of randomcolors
 startGame.addEventListener('click', playGame)
 
+function playGame(){
+    setTimeout(iterateRandomColors, 1000)
+} 
 
-function randomColor(){
+function makeRandomColor(){
     const colorPattern = []
-    for(i=0; i < lights.length + turnCount; i++){
-        const randomValue = Math.floor(Math.random() * lights.length)
-        colorPattern.push(lights[randomValue].id)
+    for(i=0; i < divColorArray.length + turnCount; i++){
+        const randomValue = Math.floor(Math.random() * divColorArray.length)
+        colorPattern.push(divColorArray[randomValue].id)
     }
     return colorPattern
 }
 
-
-function findColor(){  //pulls out each color from generateColors array then passes it as an argument through lightUpColor()
+function iterateRandomColors(){ //calls functiosn to add and remove color
     let delay = 1000
-    
     for (i = 0; i < randomColorArray.length; i++){
         const currentColor = randomColorArray[i]
-        doTimer(currentColor, delay)
+        addColorTimer(currentColor, delay)
+        removeColorTimer(currentColor, delay)
         delay += 1000
     }
 }
-
-// console.log(randomColorArray)
-// console.log(lights[0].id)
-
 // for each color
 // light it up for 1 second
 // then remove lightup
 // then increase the delay of the next lightup
 // then do next color
 
-function doTimer(color, delay) {
-    // set timeout
-    const timer = setTimeout(() => {
-        lightUpColor(color)
+function addColorTimer(color, delay) {
+    const addlightTimer = setTimeout(() => {
+        addColor(color)
     }, delay)
-
-    // delay = 1000
-    // next timer starts at 2000
-
-
-    // removeColor(color)
-
 }
 
-function lightUpColor(color) {
+function removeColorTimer(color, delay){
+    const removeLightTimer = setTimeout(() => {
+        removeColor(color)
+    }, delay + 1000)
+}
+
+function addColor(color) {
         const element = document.getElementById(color)
         element.style.backgroundColor = color
+        console.log
 }
 
 function removeColor(color){    //need to add a timer to this
     for(i=0; i<randomColorArray.length; i++){
-        if(color === lights[i].id){
-                console.log(lights[i].id) 
+        if(color === divColorArray[i].id){
+            divColorArray[i].style.opacity = .3
+            console.log(divColorArray[i], color)
             }
         }
     }
 
 
-
-function playGame(){
-    setTimeout(findColor, 1000)
-} 
+    //currently opacity is .3
+    //adds solid color over rgba
+    //adds opacity as a seperate style //blocks repeat solid
