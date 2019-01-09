d8de034f7b5badd6163f41a3b836f7c0501a74bf
Rendon Elsesser <rendonelsesser@gmail.com>
2019-01-09 17:41:36 -0500

    added timers, adjusted time so colors appear and disappear

--- src/js/main.js
+++ src/js/main.js
@@ -1,70 +1,62 @@
+const divColorArray = document.querySelectorAll(".light");
+const startGame = document.getElementById("startButton");
+const restartGame = document.getElementById("restartButton");
+const playerSelection = [];
+const availableColors = ["red", "green", "blue", "yellow"];
+let turnCount = 0;
+const randomColorArray = makeRandomColor(); // array of randomcolors
 
+startGame.addEventListener("click", playGame);
+restartGame.addEventListener('click', restartListener)
 
-const divColorArray = document.querySelectorAll(".light")
-const startGame = document.getElementById("startButton")
-const restartGame = document.getElementById("restartButton")
-const playerSelection = []
-const availableColors = ["red", "green", "blue", "yellow"]
-let turnCount = 0
-const randomColorArray = makeRandomColor() // array of randomcolors
-startGame.addEventListener('click', playGame)
-
-function playGame(){
-    setTimeout(iterateRandomColors, 1000)
-} 
-
-function makeRandomColor(){
-    const colorPattern = []
-    for(i=0; i < divColorArray.length + turnCount; i++){
-        const randomValue = Math.floor(Math.random() * divColorArray.length)
-        colorPattern.push(divColorArray[randomValue].id)
-    }
-    return colorPattern
+function playGame() {
+  setTimeout(iterateRandomColors, 1000);
 }
 
-function iterateRandomColors(){ //calls functiosn to add and remove color
-    let delay = 1000
-    for (i = 0; i < randomColorArray.length; i++){
-        const currentColor = randomColorArray[i]
-        addColorTimer(currentColor, delay)
-        removeColorTimer(currentColor, delay)
-        delay += 1000
-    }
+function makeRandomColor() {
+  const colorPattern = [];
+  for (i = 0; i < divColorArray.length + turnCount; i++) {
+    const randomValue = Math.floor(Math.random() * divColorArray.length);
+    colorPattern.push(divColorArray[randomValue].id);
+  }
+  return colorPattern;
 }
-// for each color
-// light it up for 1 second
-// then remove lightup
-// then increase the delay of the next lightup
-// then do next color
+
+function iterateRandomColors() {
+  //calls functiosn to add and remove color
+  let delay = 1000;
+  for (i = 0; i < randomColorArray.length; i++) {
+    const currentColor = randomColorArray[i];
+    addColorTimer(currentColor, delay);
+    removeColorTimer(currentColor, delay);
+    delay += 1000;
+  }
+}
 
 function addColorTimer(color, delay) {
-    const addlightTimer = setTimeout(() => {
-        addColor(color)
-    }, delay)
+  const addlightTimer = setTimeout(() => {
+    addColor(color);
+  }, delay);
 }
 
-function removeColorTimer(color, delay){
-    const removeLightTimer = setTimeout(() => {
-        removeColor(color)
-    }, delay + 1000)
+function removeColorTimer(color, delay) {
+  const removeLightTimer = setTimeout(() => {
+    removeColor(color);
+  }, delay + 900);
 }
 
 function addColor(color) {
-        const element = document.getElementById(color)
-        element.style.backgroundColor = color
-        console.log
+  const element = document.getElementById(color);
+  element.classList.add("shade");
 }
 
-function removeColor(color){    //need to add a timer to this
-    for(i=0; i<randomColorArray.length; i++){
-        if(color === divColorArray[i].id){
-            divColorArray[i].style.opacity = .3
-            console.log(divColorArray[i], color)
-            }
-        }
+function removeColor(color) {
+  //need to add a timer to this
+  for (i = 0; i < randomColorArray.length; i++) {
+    if (color === divColorArray[i].id) {
+      divColorArray[i].classList.remove("shade");
+      console.log(divColorArray[i], color);
     }
+  }
+}
 
-
-    //currently opacity is .3
-    //adds solid color over rgba
-    //adds opacity as a seperate style //blocks repeat solid
