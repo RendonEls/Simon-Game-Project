fcbe16f83723c76f0b93a40551fc3be4b03aab8c
Rendon Elsesser <rendonelsesser@gmail.com>
2019-01-11 09:01:04 -0500

    changed background image, adjusted styling

--- dist/main.31b31d70.js
+++ dist/main.31b31d70.js
@@ -176,7 +176,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"./../images/wallpaper.jpg":[["wallpaper.90dedb68.jpg","../images/wallpaper.jpg"],"../images/wallpaper.jpg"],"_css_loader":"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./../images/darkerblueBackground.jpg":[["darkerblueBackground.5aab35d1.jpg","../images/darkerblueBackground.jpg"],"../images/darkerblueBackground.jpg"],"_css_loader":"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/css-loader.js"}],"../../../../../../../usr/local/lib/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -203,7 +203,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49188" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53092" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
